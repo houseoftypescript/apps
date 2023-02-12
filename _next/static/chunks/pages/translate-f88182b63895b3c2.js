683eed5159a4f2c6cfe340bf09d2460ec5b523a9
hieudoanm <hieumdoan@gmail.com>
2023-02-12 16:12:57 +0000

    Deploying to gh-pages from @ houseofmystery/apps@d7c1f18199cc9f235056e823762654d6ac896ae5 🚀

---
+++ _next/static/chunks/pages/translate-f88182b63895b3c2.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[575],{6023:function(n,t,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/translate",function(){return u(8201)}])},8201:function(n,t,u){"use strict";u.r(t);var _=u(5893);let e=()=>(0,_.jsx)(_.Fragment,{});t.default=e}},function(n){n.O(0,[774,888,179],function(){return n(n.s=6023)}),_N_E=n.O()}]);
